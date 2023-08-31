145832b834cdb457995fd0b51e0ffa19a7742e7e
santiago <paulsantiagovaldivia@gmail.com>
2023-08-31 15:39:27 -0400

    fix: fetch data

--- components/main-la-constru/CatalogCompanies.js
+++ components/main-la-constru/CatalogCompanies.js
@@ -1,64 +1,8 @@
-import { supabase } from '@/lib/supabaseClient';
-import { useState, useEffect } from 'react';
+import React from 'react';
 import styles from './catalog.module.css';
-import { useRouter } from 'next/router';
 import Image from 'next/image';
 
-export default function CatalogCompanies({companies}) {
-  const [images, setImages] = useState({});
-  const router = useRouter();
-  
-
-
-  useEffect(() => {
-    const fetchImages = async () => {
-      const companyIds = companies.map((company) => company.id);
-      const imagePromises = companyIds.map(async (companyId) => {
-        const { data: companyData, error } = await supabase
-          .from('companies')
-          .select('imagen_portada')
-          .eq('id', companyId)
-          .single();
-
-        if (error) {
-          console.error(error);
-          return null;
-        }
-
-        const imageName = companyData.imagen_portada;
-        if (!imageName) return null;
-
-        const { data, error: downloadError } = await supabase.storage
-          .from('comp')
-          .download(`${companyId}/${imageName}`);
-
-        if (downloadError) {
-          console.error(downloadError);
-          return null;
-        }
-
-        return { companyId, data };
-      });
-
-      const downloadedImages = await Promise.all(imagePromises);
-      const images = downloadedImages.reduce((acc, image) => {
-        if (image) {
-          acc[image.companyId] = URL.createObjectURL(image.data);
-        }
-        return acc;
-      }, {});
-
-      setImages(images);
-
-    };
-
-    fetchImages();
-  }, [companies]);
-
-  const redirectToCatalog = (companyName) => {
-    router.push(`/proveedor/${encodeURIComponent(companyName)}`);
-  };
-
+export default function CatalogCompanies({ companies, images, redirectToCatalog }) {
   return (
     <div className={styles.container}>
       {companies.map((company) => {
--- pages/index.js
+++ pages/index.js
@@ -4,36 +4,86 @@ import CatalogCompanies from "@/components/main-la-constru/CatalogCompanies";
 import NavBar from "@/components/navl-la-constru/NavBar"
 import { useState, useEffect } from "react";
 import { supabase } from "@/lib/supabaseClient";
+import { useRouter } from "next/router";
+// ... (imports)
+
 export default function LaConstru() {
   const [loading, setLoading] = useState(true);
   const [companies, setCompanies] = useState([]);
-  useEffect(() => {
-    const fetchCompanies = async () => {
-      const { data, error } = await supabase.from('companies').select('*');
-      if (error) {
-        console.error(error);
-        return;
-      }
+  const [images, setImages] = useState({});
+  const router = useRouter();
 
-      setCompanies(data);
-      setLoading(false)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data: companiesData, error: companiesError } = await supabase.from('companies').select('*');
+        if (companiesError) {
+          console.error(companiesError);
+          return;
+        }
+
+        const companyIds = companiesData.map((company) => company.id);
+        const imagePromises = companyIds.map(async (companyId) => {
+          const { data: companyData, error: imageError } = await supabase
+            .from('companies')
+            .select('imagen_portada')
+            .eq('id', companyId)
+            .single();
+
+          if (imageError) {
+            console.error(imageError);
+            return null;
+          }
+
+          const imageName = companyData.imagen_portada;
+          if (!imageName) return null;
+
+          const { data, error: downloadError } = await supabase.storage
+            .from('comp')
+            .download(`${companyId}/${imageName}`);
+
+          if (downloadError) {
+            console.error(downloadError);
+            return null;
+          }
+
+          return { companyId, data };
+        });
+
+        const downloadedImages = await Promise.all(imagePromises);
+        const images = downloadedImages.reduce((acc, image) => {
+          if (image) {
+            acc[image.companyId] = URL.createObjectURL(image.data);
+          }
+          return acc;
+        }, {});
+
+        setCompanies(companiesData);
+        setImages(images);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
     };
 
-    fetchCompanies();
+    fetchData();
   }, []);
+
+  const redirectToCatalog = (companyName) => {
+    router.push(`/proveedor/${encodeURIComponent(companyName)}`);
+  };
+
   return (
     <>
-      {loading ? ( // Mostrar pantalla de carga si loading es true
+      {loading ? (
         <Loading />
       ) : (
         <>
           <NavBar />
-          <CatalogCompanies companies={companies}/>
+          <CatalogCompanies images={images} redirectToCatalog={redirectToCatalog} companies={companies} />
           <Footer />
         </>
-
       )}
-
     </>
-  )
-} 
+  );
+}
