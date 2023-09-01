055ba193c76dca4d53402e50d74a4071de01b9f3
santiago <paulsantiagovaldivia@gmail.com>
2023-09-01 18:43:21 -0400

    fix: redirect wsp link product

--- components/catalog-products/CatalogProducts.js
+++ components/catalog-products/CatalogProducts.js
@@ -4,31 +4,23 @@ import Image from 'next/image';
 import { FaWhatsapp } from 'react-icons/fa';
 
 export default function CatalogProducts({ wsp, catalogs, images }) {
-  const handleConsultClick = async (catalog) => {
+  const handleConsultClick = (catalog) => {
     // Obtén el enlace de la imagen
     const imageUrl = `https://jzmtmllsdrqaenisuxbj.supabase.co/storage/v1/object/public/img2/19/${catalog.image}`;
 
-    // Carga la imagen y la convierte en base64
-    const response = await fetch(imageUrl);
-    const blob = await response.blob();
-    const reader = new FileReader();
-    reader.readAsDataURL(blob);
-    reader.onloadend = () => {
-      const imageBase64 = reader.result.split(',')[1];
+    // Crea el mensaje de WhatsApp con la imagen oculta en formato Markdown
+    const whatsappMessage = `¡Hola Vaporwave! Me interesa comprar el producto ${catalog.name}.\n\n${catalog.description}\n\nPrecio: ${catalog.price}${catalog.currency_type}\n\n${imageUrl}`;
 
-      // Crea el mensaje de WhatsApp con la imagen en formato base64
-      const whatsappMessage = `¡Hola Vaporwave! Me interesa comprar el producto ${catalog.name}.\n\n${catalog.description}\n\nPrecio: ${catalog.price}${catalog.currency_type}\n\n![Imagen](data:image/jpeg;base64,${imageBase64})`;
+    // Codifica el mensaje para usarlo en un enlace
+    const encodedMessage = encodeURIComponent(whatsappMessage);
 
-      // Codifica el mensaje para usarlo en un enlace
-      const encodedMessage = encodeURIComponent(whatsappMessage);
+    // Crea el enlace de WhatsApp
+    const whatsappURL = `${wsp}&text=${encodedMessage}`;
 
-      // Crea el enlace de WhatsApp
-      const whatsappURL = `${wsp}&text=${encodedMessage}`;
-
-      // Abre WhatsApp en una nueva ventana o pestaña
-      window.open(whatsappURL, '_blank', 'noopener,noreferrer');
-    };
+    // Abre WhatsApp en una nueva ventana o pestaña
+    window.open(whatsappURL, '_blank', 'noopener,noreferrer');
   };
+
   return (
     <div className={styles.container}>
       {catalogs.map((catalog) => (
@@ -48,9 +40,7 @@ export default function CatalogProducts({ wsp, catalogs, images }) {
           </p>
           {wsp && (
             <button className={styles.wspButton} onClick={() => handleConsultClick(catalog)} target="_blank">
-              <span className={styles.wspIcon}>
                 <FaWhatsapp />
-              </span>
               Consultar
             </button>        
           )}
