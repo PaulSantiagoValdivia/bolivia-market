af85f4ebfbe32daad93f26be6289dd42787bb9f0
santiago <paulsantiagovaldivia@gmail.com>
2023-09-02 12:56:24 -0400

    fix: erros send messages wsp

--- components/catalog-products/CatalogProducts.js
+++ components/catalog-products/CatalogProducts.js
@@ -1,29 +1,36 @@
 import styles from './catalog.module.css';
 import Image from 'next/image';
 import { FaWhatsapp } from 'react-icons/fa';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
 export default function CatalogProducts({ wsp, catalogs, images }) {
   const [imageUrl, setImageUrl] = useState();
+
   const handleConsultClick = (catalog) => {
-  // Obtén el enlace de la imagen
-    setImageUrl(`https://jzmtmllsdrqaenisuxbj.supabase.co/storage/v1/object/public/img2/19/${catalog.image}`);
-
-    // Codifica el mensaje de WhatsApp
-    const whatsappMessage = `¡Hola Vaporwave! Me interesa comprar el producto ${catalog.name}.\n\n${catalog.description}\n\nPrecio: ${catalog.price}${catalog.currency_type}$. \n\nImagen: ${imageUrl}`;
-
-    // Codifica el mensaje y la imagen para usarlo en un enlace
-    const encodedMessage = encodeURIComponent(whatsappMessage);
-
-    // Crea el enlace de WhatsApp con la imagen adjunta
-    const whatsappURL = `${wsp}&text=${encodedMessage}`;
-
-    // Abre WhatsApp en una nueva ventana o pestaña
-    window.open(whatsappURL, '_blank', 'noopener,noreferrer');
+    if (imageUrl) {
+      const imageOnlineUrl = `https://jzmtmllsdrqaenisuxbj.supabase.co/storage/v1/object/public/img2/19/${catalog.image}`;
+      const whatsappMessage = `¡Hola Vaporwave! Me interesa comprar el producto ${catalog.name}.\n\n${catalog.description}\n\nPrecio: ${catalog.price}${catalog.currency_type}$. \n\nImagen: ${imageOnlineUrl}`;
+      const encodedMessage = encodeURIComponent(whatsappMessage);
+      const whatsappURL = `${wsp}&text=${encodedMessage}`;
+      window.open(whatsappURL, '_blank', 'noopener,noreferrer');
+    } else {
+      alert('Por favor espera a que la imagen se cargue antes de consultar.');
+    }
   };
+  
+
+  useEffect(() => {
+    if (catalogs.length > 0) {
+      // Establecer la URL de la primera imagen cuando se cargue la lista de catálogos
+      const initialImageUrl = `https://jzmtmllsdrqaenisuxbj.supabase.co/storage/v1/object/public/img2/19/${catalogs[0].image}`;
+      setImageUrl(initialImageUrl);
+    }
+  }, [catalogs]);
 
   const handleClick = (catalog) => () => {
     window.open(`https://jzmtmllsdrqaenisuxbj.supabase.co/storage/v1/object/public/img2/19/${catalog.image}`);
   };
+
   return (
     <div className={styles.container}>
       {catalogs.map((catalog) => (
@@ -36,6 +43,7 @@ export default function CatalogProducts({ wsp, catalogs, images }) {
             width={173}
             height={173}
             loading="lazy"
+            onLoad={() => setImageUrl(images[catalog.image])}
           />
           <h3 className={styles.nameProducts}>{catalog.name}</h3>
           <p className={styles.descriptionProducts}>{catalog.description}</p>
